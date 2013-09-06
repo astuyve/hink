f3b65bfa9f1a79e16ddd37b42d42eb9a2670279b
Chris Arcand <chris@chrisarcand.com>
2013-09-05 19:58:57 -0500

    Add alternate mongodb path for osx users

--- bin/start_mongo.sh
+++ bin/start_mongo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 if [ -d "/data/db/" ]; then
-  sudo mongod --fork --logappend --logpath=/var/lib/mongodb/log
+  if [ -d "/var/lib/mongodb" ]; then
+    sudo mongod --fork --logappend --logpath=/var/lib/mongodb/log
+  else
+    # Must be one of those damn OSX users...
+    mongod --fork --logappend --logpath=/usr/local/var/log/mongodb/mongo.log
+  fi
 else
   echo "directory /data/db/ is missing"
 fi

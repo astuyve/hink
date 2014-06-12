c306e1c87cfb5408cf1311bf04508fb2ae8fbc11
brady.ouren <brady.ouren@sportngin.com>
2014-06-12 15:27:07 -0500

    protect on no secret_token

--- config/initializers/defender.rb
+++ config/initializers/defender.rb
@@ -1,13 +1,17 @@
 module Defender
   def self.[](key)
     unless @config
-      raw_config = File.read("#{Rails.root}/config/defender.yml")
-      @config = YAML.load(raw_config)[Rails.env].symbolize_keys
+      begin
+        raw_config = File.read("#{Rails.root}/config/defender.yml")
+        @config = YAML.load(raw_config)[Rails.env].symbolize_keys
+      rescue
+        @config = nil
+      end
     end
-    @config[key]
+    @config[key] if @config
   end
 
   def self.[]=(key, value)
-    @config[key.to_sym] = value
+    @config.present? ? @config[key.to_sym] = value : nil
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,4 +16,5 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Hink::Application.config.secret_token = Defender[:secret_token]
+Hink::Application.config.secret_token = Defender[:secret_token] || 'asdfef234rasdf34tasdfDFef12SDFda'
+
